f4b44746864d596fc4d049d2919c884154f00e34
Dustin Griffith <djarekg@users.noreply.github.com>
2023-05-04 19:27:13 -0400

    added eslint prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "ignore",
+  printWidth: 120,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  // singleAttributesPerLine: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  plugins: ["prettier-plugin-multiline-arrays"],
+
+  overrides: [
+    {
+      files: "*.css",
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+};
