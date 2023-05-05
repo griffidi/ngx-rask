66061df763c61081ed1705d0ad087036274a4cca
Dustin Griffith <djarekg@users.noreply.github.com>
2023-05-05 02:25:07 -0400

    remove base

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { unique } from '@ngx-rask/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'demo';
+
+  constructor() {
+    const arr = [1, 2, 3, 4, 5];
+    const uniq = unique(arr);
+  }
 }
