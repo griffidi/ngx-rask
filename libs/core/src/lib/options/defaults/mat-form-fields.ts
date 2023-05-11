c1d70853a2ca6da2ec4805f2865f3ec77cc409ba
Dustin Griffith <djarekg@users.noreply.github.com>
2023-05-10 22:16:30 -0400

    fixed autofill background

--- libs/core/src/lib/options/defaults/mat-form-fields.ts
+++ libs/core/src/lib/options/defaults/mat-form-fields.ts
@@ -1,9 +1,9 @@
 import { type Provider } from '@angular/core';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, type MatFormFieldDefaultOptions } from '@angular/material/form-field';
 
 export const MAT_FORM_FIELDS: Provider = {
   provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
   useValue: {
     appearance: 'outline',
-  },
+  } as MatFormFieldDefaultOptions,
 };
